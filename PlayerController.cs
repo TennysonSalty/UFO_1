82df9afb9b87c3c2382dbef9dfb518c3e6bc751c
TennysonBranch <54789228+TennysonBranch@users.noreply.github.com>
2019-09-15 01:12:22 -0400

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+
+
+{
+    public float speed;
+    public Text countText;
+    private Rigidbody2D rb2d;
+    private int count;
+    public Text winText;
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+
+    }
+    private void Update()
+    {
+        if (Input.GetKey("escape"))
+            {
+            Application.Quit();
+            }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+    }
+    void SetCountText()
+    {
+        countText.text = "Count:" + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You win! Game created by Tennyson Branch";
+        }
+    }
+}
